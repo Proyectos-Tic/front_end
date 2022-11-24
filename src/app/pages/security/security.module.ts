fecd6192a85e8bf48b883c0e14fbefe35b54a118
Sebastian Gomez <sebastiangomezv98@gmail.com>
2022-11-24 04:36:40 -0500

    Login component html logic and module config

--- src/app/pages/security/security.module.ts
+++ src/app/pages/security/security.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { SecurityRoutingModule } from './security-routing.module';
 import { LoginComponent } from './login/login.component';
 import { NbCardModule } from '@nebular/theme';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { NbCardModule } from '@nebular/theme';
     CommonModule,
     SecurityRoutingModule,
     NbCardModule,
+    FormsModule
   ]
 })
 export class SecurityModule { }
